c2e475897f69c2277b4a52897f81c1316ab7b446
jonahminihan <jonahminihan@gmail.com>
2022-11-02 23:49:48 -0500

    added restuarantss

--- _next/static/TfV1O6O189ytlzXhiGX_z/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-cbc8dc8a38b8a6f2.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
